8473def04e2c29096bab7dea8f0e1610ef3b6bec
Michael Loschiavo <losch103@gmail.com>
2016-11-15 15:17:19 -0800

    Rendered circles on board, spaced 30 degrees. Issues with color and resizing

--- script/main.js
+++ script/main.js
@@ -1,32 +1,47 @@
-// const window_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-// const window_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+const window_width = window.innerWidth
+const window_height = window.innerHeight
 
 const width = 900;
 const height = 500;
 
-const svg = d3.select("body").append("svg")
-.attr("width", width)
-.attr("height", height)
-.style("background-color", "black");
+const svg = d3.select("svg")
+.attr("width", window_width)
+.attr("height", window_height/2);
+
+let origin = {
+  cx: window_width/2,
+  cy: window_height/4,
+  r: 40
+};
+
+// let innerCircle = svg.append("circle")
+// .attr("cy", origin.cy)
+// .attr("cx", origin.cx)
+// .attr("r", origin.r)
+// .style("fill", "green");
 
 
-let cirs_one = [
-  {"r": 10, "cx": 30, "cy": 30},
-  {"r": 10, "cx": 60, "cy": 30},
-  {"r": 10, "cx": 90, "cy": 30},
-  {"r": 10, "cx": 120, "cy": 30},
-  {"r": 10, "cx": 150, "cy": 30},
-  {"r": 10, "cx": 180, "cy": 30},
-  {"r": 10, "cx": 210, "cy": 30},
-  {"r": 10, "cx": 240, "cy": 30},
-  {"r": 10, "cx": 270, "cy": 30},
-  {"r": 10, "cx": 300, "cy": 30},
-  {"r": 10, "cx": 330, "cy": 30},
-  {"r": 10, "cx": 360, "cy": 30}
-];
+// const dotOriginX = origin.cx + ((60) * Math.sin(0));
+// const dotOriginY = origin.cy - ((60) * Math.cos(0));
 
-svg.selectAll("circle").data(cirs_one)
-.enter().append("circle")
-.attr("cy", (d,i) => d.cy)
-.attr("cx", (d,i) => d.cx)
-.attr("r", (d,i) => d.r);
+const makeCircle = (originDist, offSet, cirRadius) => {
+  let newOriginX = origin.cx + ((originDist) * Math.sin(offSet * (Math.PI/180)));
+  let newOriginY = origin.cy - ((originDist) * Math.cos(offSet * (Math.PI/180)));
+
+  for (var angle = 0; angle <= 360; angle+= 30) {
+    svg.append("circle")
+    .attr("cx", newOriginX)
+    .attr("cy", newOriginY)
+    .attr("r", cirRadius)
+    .attr("transform",`rotate(${angle}, ${origin.cx}, ${origin.cy})`)
+    .style("fill", "grey" );
+  }
+}
+
+let cir1 = makeCircle(60, 0, 5);
+let cir2 = makeCircle(90, 15, 7);
+let cir3 = makeCircle(120, 0, 9);
+let cir4 = makeCircle(150, 15, 11);
+let cir5 = makeCircle(180, 0, 13);
+
+cir1.select("circle").style("fill", "cyan")
