aa70fa1d46309ed972205329a8179d167dcf7b97
Michael Loschiavo <losch103@gmail.com>
2016-11-15 21:31:56 -0800

    Issue with initial render. Wheel rotates slowly

--- script/main.js
+++ script/main.js
@@ -1,47 +1,60 @@
-const window_width = window.innerWidth
-const window_height = window.innerHeight
+let height;
+let width;
+let cir1, cir2, cir3, cir4, cir5;
 
-const width = 900;
-const height = 500;
+const start = Date.now()
+
 
 const svg = d3.select("svg")
-.attr("width", window_width)
-.attr("height", window_height/2);
 
 let origin = {
-  cx: window_width/2,
-  cy: window_height/4,
+  cx: 250,
   r: 40
 };
-
-// let innerCircle = svg.append("circle")
-// .attr("cy", origin.cy)
-// .attr("cx", origin.cx)
-// .attr("r", origin.r)
-// .style("fill", "green");
-
-
-// const dotOriginX = origin.cx + ((60) * Math.sin(0));
-// const dotOriginY = origin.cy - ((60) * Math.cos(0));
 
 const makeCircle = (originDist, offSet, cirRadius) => {
   let newOriginX = origin.cx + ((originDist) * Math.sin(offSet * (Math.PI/180)));
   let newOriginY = origin.cy - ((originDist) * Math.cos(offSet * (Math.PI/180)));
 
-  for (var angle = 0; angle <= 360; angle+= 30) {
-    svg.append("circle")
+  let cir_group = svg.selectAll("g")
+
+  for (var angle = 0; angle < 360; angle += 30) {
+    cir_group.append("circle")
     .attr("cx", newOriginX)
     .attr("cy", newOriginY)
     .attr("r", cirRadius)
     .attr("transform",`rotate(${angle}, ${origin.cx}, ${origin.cy})`)
-    .style("fill", "grey" );
+    .style("fill", (angle === 0 ? "red" : "grey") );
   }
 }
 
-let cir1 = makeCircle(60, 0, 5);
-let cir2 = makeCircle(90, 15, 7);
-let cir3 = makeCircle(120, 0, 9);
-let cir4 = makeCircle(150, 15, 11);
-let cir5 = makeCircle(180, 0, 13);
 
-cir1.select("circle").style("fill", "cyan")
+const redraw = () => {
+
+  svg.selectAll("g").remove()
+  svg.append("g")
+  width = document.getElementById("wheel-svg").width.baseVal.value;
+  height = document.getElementById("wheel-svg").height.baseVal.value;
+
+  origin.cx = width/2
+  origin.cy = height/2
+
+
+  cir1 = makeCircle(60, 0, 5);
+  cir2 = makeCircle(90, 15, 7);
+  cir3 = makeCircle(120, 30, 9);
+  cir4 = makeCircle(150, 45, 11);
+  cir5 = makeCircle(180, 60, 13);
+
+  d3.interval(function(elapsed) {
+    svg.selectAll("g").attr("transform", `rotate(${.01 * elapsed}, ${origin.cx}, ${origin.cy})`);
+  });
+}
+
+
+
+redraw()
+
+window.addEventListener('resize', ev => {
+    redraw()
+  });
